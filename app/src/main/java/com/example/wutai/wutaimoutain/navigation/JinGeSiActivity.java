d6cf7804bcd6aebe05e79b0be4e7d01784fd1137
zhangwei <945151065@qq.com>
2018-11-07 22:05:03 +0800

    省保修改1

--- app/src/main/java/com/example/wutai/wutaimoutain/navigation/JinGeSiActivity.java
+++ app/src/main/java/com/example/wutai/wutaimoutain/navigation/JinGeSiActivity.java
@@ -11,6 +11,10 @@ import android.widget.Button;
 import android.widget.PopupMenu;
 
 import com.example.wutai.wutaimoutain.R;
+import com.example.wutai.wutaimoutain.ShengBao.JinGeSiVoice.JGSDaBeiDianActivity;
+import com.example.wutai.wutaimoutain.ShengBao.JinGeSiVoice.JGSDaXiongBaoDianActivity;
+import com.example.wutai.wutaimoutain.ShengBao.JinGeSiVoice.JGSTianWangDianActivity;
+import com.example.wutai.wutaimoutain.ShengBao.JinGeSiVoice.JGSWoFoDianActivity;
 import com.example.wutai.wutaimoutain.TaYuanSiVoice.DaBaiTaActivity;
 import com.example.wutai.wutaimoutain.TaYuanSiVoice.DaCangJingGeActivity;
 import com.example.wutai.wutaimoutain.TaYuanSiVoice.DaCiYanShouBaoDianActivity;
@@ -22,7 +26,7 @@ import com.iflytek.cloud.SpeechUtility;
 
 
 public class JinGeSiActivity extends AppCompatActivity implements View.OnClickListener,PopupMenu.OnMenuItemClickListener{
-    private Button DCJG,DBT,DCYSBD,TWD;
+    private Button WFD,DXBD,DBD,TWD;
     private String content;
     private Context mContex;
     private String name = "金阁寺";
@@ -37,49 +41,49 @@ public class JinGeSiActivity extends AppCompatActivity implements View.OnClickLi
     public void initView(){
         //初始化声音设置
         SpeechUtility.createUtility(JinGeSiActivity.this, SpeechConstant.APPID +"=5b63c383");  //=后面这里要替换成自己申请的 AppID
-        DCJG = (Button)findViewById(R.id.da_cang_jing_ge);
-        DCJG.setOnClickListener(this);
-        DBT = (Button)findViewById(R.id.da_bai_ta);
-        DBT.setOnClickListener(this);
-        DCYSBD = (Button)findViewById(R.id.da_ci_bao_dian);
-        DCYSBD.setOnClickListener(this);
-        TWD = (Button)findViewById(R.id.tian_wang_dian);
+        WFD = (Button)findViewById(R.id.JGS_wo_fo_dian);
+        WFD.setOnClickListener(this);
+        DXBD = (Button)findViewById(R.id.JGS_da_xiong_bao_dian);
+        DXBD.setOnClickListener(this);
+        DBD = (Button)findViewById(R.id.JGS_da_bei_dian);
+        DBD.setOnClickListener(this);
+        TWD = (Button)findViewById(R.id.JGS_tian_wang_dian);
         TWD.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.da_cang_jing_ge:
+            case R.id.JGS_wo_fo_dian:
                 //创建弹出式菜单对象（最低版本11）
                 PopupMenu popup = new PopupMenu(this, v);//第二个参数是绑定的那个view
                 //获取菜单填充器
                 MenuInflater inflater = popup.getMenuInflater();
                 //填充菜单
-                inflater.inflate(R.menu.da_cang_jing_ge_menu, popup.getMenu());
+                inflater.inflate(R.menu.jgs_wo_fo_dian_menu, popup.getMenu());
                 //绑定菜单项的点击事件
                 popup.setOnMenuItemClickListener(JinGeSiActivity.this);
                 //显示(这一行代码不要忘记了)
                 popup.show();
                 break;
-            case R.id.da_bai_ta:
+            case R.id.JGS_da_xiong_bao_dian:
                 PopupMenu popup2 = new PopupMenu(this,v);
                 MenuInflater inflater2 = popup2.getMenuInflater();
-                inflater2.inflate(R.menu.da_bai_ta_menu,popup2.getMenu());
+                inflater2.inflate(R.menu.jgs_da_xiong_bao_dian_menu,popup2.getMenu());
                 popup2.setOnMenuItemClickListener(JinGeSiActivity.this);
                 popup2.show();
                 break;
-            case R.id.da_ci_bao_dian:
+            case R.id.JGS_da_bei_dian:
                 PopupMenu popup3 = new PopupMenu(this,v);
                 MenuInflater inflater3 = popup3.getMenuInflater();
-                inflater3.inflate(R.menu.da_ci_yan_shou_bao_dian_menu,popup3.getMenu());
+                inflater3.inflate(R.menu.jgs_da_bei_dian_menu,popup3.getMenu());
                 popup3.setOnMenuItemClickListener(JinGeSiActivity.this);
                 popup3.show();
                 break;
-            case R.id.tian_wang_dian:
+            case R.id.JGS_tian_wang_dian:
                 PopupMenu popup4 = new PopupMenu(this,v);
                 MenuInflater inflater4 = popup4.getMenuInflater();
-                inflater4.inflate(R.menu.tian_wang_dian_menu,popup4.getMenu());
+                inflater4.inflate(R.menu.jgs_tian_wang_dian_menu,popup4.getMenu());
                 popup4.setOnMenuItemClickListener(JinGeSiActivity.this);
                 popup4.show();
                 break;
@@ -91,41 +95,41 @@ public class JinGeSiActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.DCJG_yinglian_detail:
-                YinglianShowActivity.actionstart(JinGeSiActivity.this,name,"大藏经阁");
+            case R.id.WFD_yinglian_detail:
+                YinglianShowActivity.actionstart(JinGeSiActivity.this,name,"金阁寺卧佛殿");
                 break;
-            case R.id.DCJG_voice_desc:
-                startActivity(new Intent(JinGeSiActivity.this, DaCangJingGeActivity.class));
+            case R.id.WFD_voice_desc:
+                startActivity(new Intent(JinGeSiActivity.this, JGSWoFoDianActivity.class));
                 break;
-            case R.id.DCJG_content:
-                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"藏经阁");
+            case R.id.WFD_content:
+                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"金阁寺卧佛殿");
                 break;
-            case R.id.DBT_yinglian_detail:
-                YinglianShowActivity  .actionstart(JinGeSiActivity.this,name,"大白塔");
+            case R.id.DXBD_yinglian_detail:
+                YinglianShowActivity  .actionstart(JinGeSiActivity.this,name,"金阁寺大雄宝殿");
                 break;
-            case R.id.DBT_voice_desc:
-                startActivity(new Intent(JinGeSiActivity.this, DaBaiTaActivity.class));
+            case R.id.DXBD_voice_desc:
+                startActivity(new Intent(JinGeSiActivity.this, JGSDaXiongBaoDianActivity.class));
                 break;
-            case R.id.DBT_content:
-                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"大白塔");
+            case R.id.DXBD_content:
+                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"金阁寺大雄宝殿");
                 break;
-            case R.id.DCBD_yinglian_detail:
-                YinglianShowActivity  .actionstart(JinGeSiActivity.this,name,"大慈延寿宝殿");
+            case R.id.DBD_yinglian_detail:
+                YinglianShowActivity  .actionstart(JinGeSiActivity.this,name,"金阁寺大悲殿");
                 break;
-            case R.id.DCBD_voice_desc:
-                startActivity(new Intent(JinGeSiActivity.this, DaCiYanShouBaoDianActivity.class));
+            case R.id.DBD_voice_desc:
+                startActivity(new Intent(JinGeSiActivity.this, JGSDaBeiDianActivity.class));
                 break;
-            case R.id.DCBD_content:
-                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"大慈延寿宝殿");
+            case R.id.DBD_content:
+                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"金阁寺大悲殿");
                 break;
             case R.id.TWD_yinglian_detail:
-                YinglianShowActivity  .actionstart(JinGeSiActivity.this,name,"天王殿");
+                YinglianShowActivity  .actionstart(JinGeSiActivity.this,name,"金阁寺天王殿");
                 break;
             case R.id.TWD_voice_desc:
                 startActivity(new Intent(JinGeSiActivity.this, TianWangDianActivity.class));
                 break;
             case R.id.TWD_content:
-                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"天王殿");
+                Jianjie_dadian_actiivty.actionstart1(JinGeSiActivity.this,name,"金阁寺天王殿");
                 break;
             default:
                 break;
