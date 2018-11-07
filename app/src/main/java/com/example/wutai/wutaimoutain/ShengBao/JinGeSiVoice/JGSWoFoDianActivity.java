dc2c60c241a2f8aeb639c37cd37dd33228140c6a
zhangwei <945151065@qq.com>
2018-11-07 21:38:02 +0800

    增加省保内容

---
+++ app/src/main/java/com/example/wutai/wutaimoutain/ShengBao/JinGeSiVoice/JGSWoFoDianActivity.java
@@ -0,0 +1,13 @@
+package com.example.wutai.wutaimoutain.ShengBao.JinGeSiVoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JGSWoFoDianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jgs_wo_fo_dian);
+    }
+}
---
+++ app/src/main/java/com/example/wutai/wutaimoutain/ShengBao/LongQuanSiVoice/LQSWuGuanTangActivity.java
@@ -0,0 +1,13 @@
+package com.example.wutai.wutaimoutain.ShengBao.LongQuanSiVoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LQSWuGuanTangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lqs_wu_guan_tang);
+    }
+}
